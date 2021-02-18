512c7df0ad088545477d844259d0261518d5838d
Serge K <cmt@phnx47.net>
2021-02-19 01:05:51 +0700

    feat: update clean-branches.sh

--- clean-branches.sh
+++ clean-branches.sh
@@ -6,27 +6,27 @@ function green_echo {
     echo -e "\e[32m${1}\e[0m"
 }
 
-regex='master$'
-#regex='master\|develop$'
+branch=master
+ignore="master\|develop$"
 
-# This has to be run from master
-git checkout master
+# This has to be run from $branch
+git checkout $branch
 
 # Update our list of remotes
 git fetch
 git remote prune origin
 
 # Remove local fully merged branches
-git branch --merged master | grep -v $regex | xargs git branch -d
+git branch --merged $branch | grep -v $ignore | xargs git branch -d
 
 # Show remote fully merged branches
 echo "The following remote branches are fully merged and will be removed:"
-git branch -r --merged master | sed 's/ *origin\///' | grep -v $regex
+git branch -r --merged $branch | sed 's/ *origin\///' | grep -v $ignore
 
 read -p "Continue (y/n)? "
 if [ "$REPLY" == "y" ]
 then
    # Remove remote fully merged branches
-   git branch -r --merged master | sed 's/ *origin\///' | grep -v $regex | xargs -I% git push origin :%
+   git branch -r --merged $branch | sed 's/ *origin\///' | grep -v $ignore | xargs -I% git push origin :%
    green_echo "Obsolete branches are removed"
 fi
