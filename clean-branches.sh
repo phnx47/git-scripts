c6ba964e7171f251e18512cb6add6738643a8b52
Serge K <cmt@phnx47.net>
2021-12-30 22:26:36 +0700

    fix: bash warning by idea

--- clean-branches.sh
+++ clean-branches.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-source $(dirname "$0")/echo.sh
+source "$(dirname "$0")/echo.sh"
 
 branch=${1}
 
@@ -16,22 +16,22 @@ yellow_echo "Branches: '${branch}', 'develop' and 'dev' will not be deleted"
 ignore="$branch\|develop\|dev$"
 
 # This has to be run from $branch
-git checkout $branch
+git checkout "$branch"
 
 # Update our list of remotes
 git fetch
 git remote prune origin
 
 # Remove local fully merged branches
-git branch --merged $branch | grep -v $ignore | xargs git branch -d
+git branch --merged "$branch" | grep -v "$ignore" | xargs git branch -d
 
 # Show remote fully merged branches
 yellow_echo "The following remote branches are fully merged and will be removed:"
-git branch -r --merged $branch | sed 's/ *origin\///' | grep -v $ignore
+git branch -r --merged "$branch" | sed 's/ *origin\///' | grep -v "$ignore"
 
-read -p "Continue (y/n)? "
+read -rp "Continue (y/n)? "
 if [ "$REPLY" == "y" ]; then
   # Remove remote fully merged branches
-  git branch -r --merged $branch | sed 's/ *origin\///' | grep -v $ignore | xargs -I% git push origin :%
+  git branch -r --merged "$branch" | sed 's/ *origin\///' | grep -v "$ignore" | xargs -I% git push origin :%
   green_echo "Obsolete branches are removed"
 fi
--- pull-all.sh
+++ pull-all.sh
@@ -4,7 +4,7 @@ remote_branches=$(git branch -r | grep -v '\->')
 
 for remote_branch in $remote_branches; do
   name=${remote_branch#origin/}
-  git branch --track $name $remote_branch || true
+  git branch --track "$name" "$remote_branch" || true
 done
 
 git fetch --all
