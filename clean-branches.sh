61907da87fdbf68537574ea460337dfaafd7fdce
Serge K <cmt@phnx47.net>
2023-05-03 21:22:25 +0700

    feat: add deletealll-local-branches.sh

--- clean-branches.sh
+++ clean-branches.sh
@@ -4,8 +4,6 @@ set -eu -o pipefail
 
 source "$(dirname "$0")/echo.sh"
 
-green_echo "Clean branches started"
-
 default_branch="$(git remote show origin | grep 'HEAD branch' | cut -d' ' -f5)"
 blue_echo "Default branch is '${default_branch}'"
 
@@ -16,31 +14,33 @@ git checkout "${default_branch}"
 # Update our list of remotes
 git fetch
 git remote prune origin
+green_echo "All remote branches pruned!"
 
 # Remove local fully merged branches
-light_green_echo "Removing local branches..."
 local_branches="$(git branch --merged "${default_branch}" | tr -d " *" | grep -vE "${ignore}" || true)"
 if [ -z "$local_branches" ]; then
   blue_echo "No local branches to delete"
 else
+  blue_echo "Deleting local branches..."
   yellow_echo "${local_branches}"
   read -rp "Delete these local branches (y/n)?"
   if [ "$REPLY" == "y" ]; then
     echo "${local_branches}" | xargs git branch -d
+    green_echo "All local branches deleted!"
   fi
 fi
 
 # Remove remote fully merged branches
-light_green_echo "Removing remote branches..."
 remote_branches="$(git branch -r --merged "${default_branch}" | sed 's/ *origin\///' | grep -vE "${ignore}" || true)"
 if [ -z "$remote_branches" ]; then
   blue_echo "No remote branches to delete"
 else
+  blue_echo "Deleting remote branches..."
   yellow_echo "${remote_branches}"
   read -rp "Delete these remote branches (y/n)?"
   if [ "$REPLY" == "y" ]; then
     echo "${remote_branches}" | xargs -I% git push origin :%
+    green_echo "All remote branches deleted!"
   fi
 fi
 
-green_echo "Clean branches finished!"
--- echo.sh
+++ echo.sh
@@ -8,10 +8,6 @@ function blue_echo {
   echo -e "\e[34m${1}\e[0m"
 }
 
-function light_green_echo {
-  echo -e "\e[92m${1}\e[0m"
-}
-
 function yellow_echo {
   echo -e "\e[33m${1}\e[0m"
 }
